65ff804a55eca7ab6eae8c9d6fbc4e09dfb0d1e7
Christoph Koehler <christoph@webempoweredchurch.org>
2008-07-11 21:39:50 +0000

    add function wizard for our importer

--- ext_tables.php
+++ ext_tables.php
@@ -7,5 +7,13 @@ $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['wecapi']);
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/rss2/', 'WEC RSS 2.0 Feed' );
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/itunes/', 'WEC iTunes Compatible Feed' );
 
-
+if (TYPO3_MODE=='BE')	{
+	t3lib_extMgm::insertModuleFunction(
+		'web_func',
+		'tx_wecapi_importwizard',
+		t3lib_extMgm::extPath($_EXTKEY).'func_wizards/class.tx_wecapi_importwizard.php',
+		'Hello!',
+		'wiz'
+	);
+}
 ?>
---
+++ func_wizards/class.tx_wecapi_importwizard.php
@@ -0,0 +1,48 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2005-2008 Christian Technology Ministries International Inc.
+* All rights reserved
+*
+* This file is part of the Web-Empowered Church (WEC)
+* (http://WebEmpoweredChurch.org) ministry of Christian Technology Ministries 
+* International (http://CTMIinc.org). The WEC is developing TYPO3-based
+* (http://typo3.org) free software for churches around the world. Our desire
+* is to use the Internet to help offer new life through Jesus Christ. Please
+* see http://WebEmpoweredChurch.org/Jesus.
+*
+* You can redistribute this file and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation;
+* either version 2 of the License, or (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This file is distributed in the hope that it will be useful for ministry,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the file!
+***************************************************************/
+
+
+require_once(PATH_t3lib.'class.t3lib_extobjbase.php');
+
+class tx_wecapi_importwizard extends t3lib_extobjbase {
+	
+	/**
+	 * Returns the menu array
+	 *
+	 * @return	array
+	 */
+	function modMenu()	{
+		return "Hello!";
+	}
+	
+	function main()	{
+		return "Hello!";
+	}
+}
+?>
